cf98690d8d39a2fd818dc418dd1e8a8bb202af8a
ApoorvaSingh15 <apoorvasingh2796@gmail.com>
2023-11-01 15:40:55 +0530

    Modified Day 52 Contact Us page | Smtp functionality to receive details
    Initial commit

---
+++ Day53_Contact-Form/main.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template,request
+import requests
+import os
+import smtplib
+
+posts = requests.get("https://api.npoint.io/178a53a246d635bbdf4d").json()
+EMAIL = os.environ.get("MY_EMAIL")
+PASSWORD = os.environ.get("MY_PASS")
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(EMAIL, PASSWORD)
+        connection.sendmail(EMAIL, EMAIL, email_message)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
